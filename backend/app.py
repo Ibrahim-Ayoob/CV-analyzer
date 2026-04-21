981948f71ae057ab0926ffc895237c33560fe1dc
Ibrahim-Ayoob <I.Mohammed2357@nu.edu.eg>
2026-04-21 23:20:41 +0200

    Add file upload route

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return "Backend is running"
 
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files["cv"]
+    return "File received"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
